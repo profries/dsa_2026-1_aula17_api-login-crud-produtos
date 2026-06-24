de1e6a07c1c5e34d4cc3165a8c181cf6bc5f7467
LUIS HENRIQUE LEAL RIES <98657828049@senacrs.edu.br>
2026-06-23 21:34:39 -0300

    Removido o buscar usuario por id

--- router/usuario_router.js
+++ router/usuario_router.js
@@ -6,6 +6,5 @@ const usuarioController = require('../controller/usuario_controller')
 //endpoint: /usuarios
 router.get('/', usuarioController.listar)
 router.post('/', usuarioController.inserir)
-router.get('/:id', usuarioController.buscarPorId)
 
 module.exports = router;
